e2a3f20328643b03ef4fe378e5b4b635e4c5028b
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-06-21 23:38:52 +0300

    driver pool

--- src/test/java/com/fidexio/utilities/Driver.java
+++ src/test/java/com/fidexio/utilities/Driver.java
@@ -30,6 +30,7 @@ public class Driver {
 
     private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
+
     /*
     Create a re-usable utility method which will return same driver instance when we call it
      */
